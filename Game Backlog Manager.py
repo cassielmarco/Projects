f03f3dbb989a2fbd8b3b06b64af7ec49fdc18164
cassielmarco <cassielmarcocervobianco@gmail.com>
2026-08-23 18:57:16 +0100

    bunch has chnaged

--- Game Backlog Manager.py
+++ Game Backlog Manager.py
@@ -1,27 +1,31 @@
 games = []
+genres = set()
 while True :
     c = input(" add / view / count / remove / edit / exit :  ")
     if c == "add" :
-        games.append(input())
+        games.append(input("Game name : "))
+        genres.add(input("Game genre :"))
     elif c == "view" :
         for game in games :
-            print(game)
+            print("Game name :"+ game[0], "Game genre :" + game[1])
     elif c == "count" :
         print(len(games))
     elif c == "edit" or c == "remove" :
         while True :
             g = input(" wich one to remove ")
-            if g in games :
-                if c == "remove" :
-                    games.remove(g)
+            for tuplegame in games:
+                if c == "remove" and g == tuplegame[0] :
+                    games.remove(tuplegame)
                     break
-                if c == "edit" :
-                    gn = input(" add ur new game ")
-                    games.remove(g)
-                    games.append(gn)
+                elif c == "edit" and g == tuplegame[0] :
+                    games.remove(tuplegame)
+                    games.append((input("New Game name :"), input("New genre: ")))
                     break
             else:
                 print(" This game doesnt exist try again ")
+            if g == tuplegame[0]:
+                break
+            
     elif c == "exit" :
         break
     else :
